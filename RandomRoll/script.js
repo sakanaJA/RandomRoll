bc273c1a48f8a4c055d1b505dedf5ff9deb3af28
Haku <89026040+sakanaJA@users.noreply.github.com>
2023-07-23 13:56:01 +0900

    T

--- RandomRoll/script.js
+++
@@ -1,7 +0,0 @@
-var options = ["MID", "TOP", "JG", "SUP", "ADC"];
-
-function chooseRandom() {
-  var randomIndex = Math.floor(Math.random() * options.length);
-  var randomChoice = options[randomIndex];
-  document.getElementById('result').textContent = "君が行くのは: " + randomChoice;
-}
